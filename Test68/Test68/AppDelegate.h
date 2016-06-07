3cb1e0bc214508a18bcb21711c99478ddeb9d29c
997419642 <997419642@qq.com>
2016-06-07 10:05:20 +0800

    test68

---
+++ Test68/Test68/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test68
+//
+//  Created by mac on 16/6/7.
+//  Copyright © 2016年 AS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
